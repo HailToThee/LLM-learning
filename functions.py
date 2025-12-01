bc1744be846823049c4dcc13b0540957d6d5f24f
HailToThee <3206104008@qq.com>
2025-12-02 00:06:23 +0800

    Instruction finetuning

--- functions.py
+++ functions.py
@@ -239,6 +239,30 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         idx = torch.cat((idx, idx_next), dim=1)  # Append the new token to the sequence
     return idx
 
+def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_idx=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]  # Keep only the last context_size tokens
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]  # Get the logits for the last token
+
+        if top_k is not None:
+            top_k_values, _ = torch.topk(logits, top_k)
+            min_top_k = top_k_values[:, -1].unsqueeze(1)
+            logits = torch.where(logits < min_top_k, torch.full_like(logits, float('-inf')), logits)
+
+        if temperature > 0.0:
+            logits = logits / temperature
+        
+        probs = torch.softmax(logits, dim=-1)  # Convert logits to probabilities
+        idx_next = torch.multinomial(probs, num_samples=1)
+        if eos_idx is not None:
+            if (idx_next == eos_idx).any():
+                break
+        idx = torch.cat((idx, idx_next), dim=1)  # Append the new token to the sequence
+    return idx
+
+
 def assign(left, right):
     if left.shape != right.shape:
         raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
@@ -305,3 +329,108 @@ def load_weights_into_gpt(gpt, params):
     gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
     gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
     gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+
+def text_to_token_ids(text,tokenizer):
+    encoded = tokenizer.encode(text,allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded, dtype=torch.long).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    decoded = tokenizer.decode(token_ids.squeeze(0).tolist())
+    return decoded
+
+
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1), target_batch.flatten()
+    )
+    return loss
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    model.eval()
+    total_loss = 0.0
+    num_batches = num_batches or len(data_loader)
+    
+    with torch.no_grad():
+        for i, (input_batch, target_batch) in enumerate(data_loader):
+            if i >= num_batches:
+                break
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+    
+    return total_loss / num_batches
+
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter,
+                       start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+    for epoch in range(num_epochs):
+        model.train()
+        for i, (input_batch, target_batch) in enumerate(train_loader):
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            
+            tokens_seen += input_batch.numel()
+            global_step += 1
+            
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                
+                print(f"Epoch {epoch}, Step {global_step}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Tokens Seen: {tokens_seen}")
+        generate_and_print_sample(model, tokenizer, device, start_context)
+    return train_losses, val_losses, track_tokens_seen
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(model=model,
+                                        idx=encoded,
+                                        max_new_tokens=50,
+                                        context_size=context_size)
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
+
+
+import matplotlib.pyplot as plt
+
+def plot_values(epochs_seen, examples_seen, train_values, val_values, label="loss"):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    # Plot training and validation loss against epochs
+    ax1.plot(epochs_seen, train_values, label=f"Training {label}")
+    ax1.plot(epochs_seen, val_values, linestyle="-.", label=f"Validation {label}")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel(label.capitalize())
+    ax1.legend()
+
+    # Create a second x-axis for tokens seen
+    ax2 = ax1.twiny()  # Create a second x-axis that shares the same y-axis
+    ax2.plot(examples_seen, train_values, alpha=0)  # Invisible plot for aligning ticks
+    ax2.set_xlabel("Examples seen")
+
+    fig.tight_layout()  # Adjust layout to make room
+    plt.savefig(f"{label}-plot.pdf")
+    plt.show()
+    
